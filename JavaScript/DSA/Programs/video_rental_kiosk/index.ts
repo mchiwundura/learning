fa592fd02f2e739063def7c88e31d815ccb932ec
mchiwundura <mchiwundura@protonmail.com>
2026-01-03 03:43:41 +0200

    Javascript video kiosk learning classes again with the get function and implementing display list function

--- JavaScript/DSA/Programs/video_rental_kiosk/index.ts
+++ JavaScript/DSA/Programs/video_rental_kiosk/index.ts
@@ -1,9 +1,58 @@
+import List from "../../ADTs/list.ts";
+
+class Customer {
+	private name: string;
+	private movie: string;
+
+	constructor(name: string, movie: string) {
+		this.name = name;
+		this.movie = movie;
+	}
+
+	get getName(): string {
+		return this.name;
+	}
+
+	get getMovie(): string {
+		return this.movie;
+	}
+}
+
+class Movie {
+	private title: string;
+	constructor(title: string) {
+		this.title = title;
+	}
+	get getName(): string {
+		return this.title;
+	}
+}
+
+function displayList(list: List<Customer | Movie>): void {
+	for (list.front(); list.currPos() < list.length(); list.next()) {
+		const el = list.getElement();
+		if (!el) continue;
+
+		if (el instanceof Customer) {
+			console.log(el.getName + ", " + el.getMovie);
+		} else {
+			console.log(el);
+		}
+	}
+}
+
 // Read from file
 import fs from "node:fs/promises";
 let moviesRaw = await fs.readFile("./films.txt", "utf-8");
-let movies = moviesRaw.split("\n");
-let movieArray: string[] = [];
-for (let [index, movie] of movies.entries()) {
-	movieArray[index] = movie.trim();
+
+let movies: string[] = [];
+for (let [index, movie] of moviesRaw.split("\n").entries()) {
+	movies[index] = movie.trim();
 }
-console.log(movieArray);
+
+let movieList = new List();
+for (let i = 0; i < movies.length; ++i) {
+	movieList.append(movies[i]);
+}
+
+console.log(movieList);
