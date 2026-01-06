adf28db7e5a8ef3a6e1f204b2ad357ff6baa832b
mchiwundura <mchiwundura@protonmail.com>
2026-01-06 20:15:56 +0200

    added a rented movies list and functionality

--- JavaScript/DSA/Programs/video_rental_kiosk/index.ts
+++ JavaScript/DSA/Programs/video_rental_kiosk/index.ts
@@ -1,45 +1,45 @@
 import List from "../../ADTs/list.ts";
 
 class Customer {
-  private name: string;
-  private movie: string;
+	private name: string;
+	private movie: string;
 
-  constructor(name: string, movie: string) {
-    this.name = name;
-    this.movie = movie;
-  }
+	constructor(name: string, movie: string) {
+		this.name = name;
+		this.movie = movie;
+	}
 
-  get getName(): string {
-    return this.name;
-  }
+	get getName(): string {
+		return this.name;
+	}
 
-  get getMovie(): string {
-    return this.movie;
-  }
+	get getMovie(): string {
+		return this.movie;
+	}
 }
 
 class Movie {
-  private title: string;
-  constructor(title: string) {
-    this.title = title;
-  }
-  get getName(): string {
-    return this.title;
-  }
+	private title: string;
+	constructor(title: string) {
+		this.title = title;
+	}
+	get getName(): string {
+		return this.title;
+	}
 }
 
 function displayList(list: List<Customer | Movie>): void {
-  for (let i = 0; i < list.length(); i++) {
-    const el = list.getElement();
-    if (!el) continue;
+	for (let i = 0; i < list.length(); i++) {
+		const el = list.getElement();
+		if (!el) continue;
 
-    if (el instanceof Customer) {
-      console.log(el.getName + ", " + el.getMovie);
-    } else {
-      console.log(el);
-    }
-    list.next();
-  }
+		if (el instanceof Customer) {
+			console.log(el.getName + ", " + el.getMovie);
+		} else {
+			console.log(el);
+		}
+		list.next();
+	}
 }
 
 // Read from file
@@ -48,26 +48,29 @@ let moviesRaw = await fs.readFile("./films.txt", "utf-8");
 
 let movies: string[] = [];
 for (let [index, movie] of moviesRaw.split("\n").entries()) {
-  movies[index] = movie.trim();
+	movies[index] = movie.trim();
 }
 
 let movieList = new List();
 for (let i = 0; i < movies.length; ++i) {
-  movieList.append(movies[i]);
+	movieList.append(movies[i]);
 }
 
 var customers = new List<Customer>();
+var rentedMovies = new List<Movie>();
 
 function checkOut(name, movie, filmList, customerList) {
-  if (movieList.contains(movie)) {
-    var c = new Customer(name, movie);
-    customers.append(c);
-    filmList.remove(movie);
-  } else {
-    console.log(movie + " is not available.");
-  }
-  movieList.front();
-  customers.front();
+	if (movieList.contains(movie)) {
+		var c = new Customer(name, movie);
+		customers.append(c);
+		rentedMovies.append(movie);
+		filmList.remove(movie);
+	} else {
+		console.log(movie + " is not available.");
+	}
+	movieList.front();
+	customers.front();
+	rentedMovies.front();
 }
 
 console.log("Available movies:");
@@ -79,5 +82,7 @@ var movie = prompt();
 checkOut(name, movie, movieList, customers);
 console.log("\nCustomer Rentals: \n");
 displayList(customers);
+console.log("\nRented Movies\n");
+displayList(rentedMovies);
 console.log("\nMovies Now Available\n");
 displayList(movieList);
