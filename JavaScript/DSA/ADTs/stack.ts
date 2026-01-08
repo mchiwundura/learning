313404ba48e7b40a163964951bcccd756d0d6cc6
mchiwundura <mchiwundura@protonmail.com>
2026-01-08 18:49:13 +0200

    created the stack abstract data type

---
+++ JavaScript/DSA/ADTs/stack.ts
@@ -0,0 +1,31 @@
+export default class Stack<T> {
+	private dataStore: T[] = [];
+	private top: number = 0;
+
+	push(element: T): void {
+		this.dataStore[this.top++] = element;
+	}
+
+	pop(): T | undefined {
+		if (this.top === 0) {
+			return undefined;
+		}
+		return this.dataStore[--this.top];
+	}
+
+	peek(): T | undefined {
+		if (this.top === 0) {
+			return undefined;
+		}
+		return this.dataStore[this.top - 1];
+	}
+
+	clear(): void {
+		this.top = 0;
+		this.dataStore = [];
+	}
+
+	length(): number {
+		return this.top;
+	}
+}
